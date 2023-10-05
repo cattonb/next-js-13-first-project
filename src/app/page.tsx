744fdca30f0cf95c43124af414e0c33ecb4ef7b0
ben <ben21catton@gmail.com>
2023-10-05 21:48:01 +0100

    added delete function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { prisma } from "@/db";
 import Link from "next/link";
+import { revalidatePath } from "next/cache";
 
 import TodoItem from "@/components/TodoItem";
 
@@ -13,12 +14,18 @@ async function toggleTodo(id: string, complete: boolean) {
   await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
+async function deleteTodo(id: string) {
+  "use server";
+  await prisma.todo.delete({ where: { id } });
+  revalidatePath("/");
+}
+
 export default async function Home() {
   const todos = await getTodos();
 
   return (
     <>
-      <header className="flex justify-between mb-2 items-center">
+      <header className="flex justify-between mb-3 items-center">
         <h1 className="text-2xl">To Do's</h1>
         <Link
           className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
@@ -26,9 +33,9 @@ export default async function Home() {
           New
         </Link>
       </header>
-      <ul className="pl-4">
+      <ul className="px-4">
         {todos.map((todo) => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,23 +5,31 @@ type TodoItemProps = {
   title: string;
   complete: boolean;
   toggleTodo: (id: string, complete: boolean) => void;
+  deleteTodo: (id: string) => void;
 };
 
-const TodoItem = ({ id, title, complete, toggleTodo }: TodoItemProps) => {
+const TodoItem = ({ id, title, complete, toggleTodo, deleteTodo }: TodoItemProps) => {
   return (
-    <li className="flex gap-1 items-center">
-      <input
-        id={id}
-        type="checkbox"
-        className="cursor-pointer peer"
-        defaultChecked={complete}
-        onChange={(e) => toggleTodo(id, e.target.checked)}
-      />
-      <label
-        htmlFor={id}
-        className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
-        {title}
-      </label>
+    <li className="flex items-center justify-between mt-2">
+      <div className="flex gap-2">
+        <input
+          id={id}
+          type="checkbox"
+          className="cursor-pointer peer"
+          defaultChecked={complete}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <label
+          htmlFor={id}
+          className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">
+          {title}
+        </label>
+      </div>
+      <button
+        className="border border-slate-300 bg-transparent rounded px-1 outline-none focus-within:border-slate-100 hover:bg-slate-700 cursor-pointer"
+        onClick={() => deleteTodo(id)}>
+        X
+      </button>
     </li>
   );
 };
